7311fead727a15569886841e16b3890f5cee9284
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-28 21:42:46 -0800

    add loops with Razor & multiple controllers

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,35 +1,13 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
     }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    // this route is invoked when form is submitted
-    // str param (description) matches the `name` attribute 
-    {
-      Item myItem = new Item(description);
-      //return View("Index", myItem);
-      // instead of `View()`, because no longer routing to a view with the same exact name as our route method
-      return RedirectToAction("Index");
-    }
-
   }
 }
---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    // this route is invoked when form is submitted
+    // str param (description) matches the `name` attribute 
+    {
+      Item myItem = new Item(description);
+      //return View("Index", myItem);
+      // instead of `View()`, because no longer routing to a view with the same exact name as our route method
+      return RedirectToAction("Index");
+    }
+
+  }
+}
