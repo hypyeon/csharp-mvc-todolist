59e15f6b010ec753b34fffd32b718716a3840550
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-11 23:21:06 -0700

    add CREATE functionality

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -32,5 +32,64 @@ namespace ToDoList.Controllers
         .FirstOrDefault(t => t.TagId == id);
       return View(tag);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Tag tag)
+    {
+      _db.Tags.Add(tag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddItem(int id)
+    {
+      Tag tag = _db.Tags.FirstOrDefault(t => t.TagId == id);
+      ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Description");
+      return View(tag);
+    }
+    [HttpPost]
+    public ActionResult AddItem(Tag tag, int itemId)
+    {
+      #nullable enable
+      ItemTag? joinEntity = _db.ItemTags
+        .FirstOrDefault(join => 
+          (join.ItemId == itemId && join.TagId == tag.TagId)
+        );
+      #nullable disable
+      if (joinEntity == null && itemId != 0)
+      {
+        _db.ItemTags.Add(new ItemTag() { ItemId = itemId, TagId = tag.TagId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = tag.TagId });
+    }
+
+    public ActionResult AddTag(int id)
+    {
+      Item item = _db.Items
+        .FirstOrDefault(i => i.ItemId == id);
+      ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Title");
+      return View(item);
+    }
+    [HttpPost]
+    public ActionResult AddTag(Item item, int tagId)
+    {
+      #nullable enable
+      ItemTag? joinEntity = _db.ItemTags
+        .FirstOrDefault(join => 
+          (join.TagId == tagId && join.ItemId == item.ItemId)
+        );
+      #nullable disable
+      if (joinEntity == null && tagId != 0)
+      {
+        _db.ItemTags.Add(new ItemTag() { TagId = tagId, ItemId = item.ItemId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = item.ItemId });
+    }  
   }
 }
